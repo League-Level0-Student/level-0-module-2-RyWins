bb4ce78f51c71138ac459b680c9dcda0af2fac4d
league <54760319+OceanNut420@users.noreply.github.com>
2022-11-03 19:51:39 -0700

    working on 8 ball

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -12,14 +12,32 @@ public class Validation {
 		
 		Random randomMaker = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
-		
-		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
-		// 2. Repeat all the code above 10 times
 		
+		// 2. Repeat all the code above 10 times
+		for ( int i = 0; i <= 10 ; i += 1) {
+			int randomNumber = randomMaker.nextInt(6);
+			System.out.println(randomNumber);
+			if (randomNumber == 1) {
+				JOptionPane.showMessageDialog(null, "I like your eyes");
+			}
+			else if (randomNumber == 2) {
+				JOptionPane.showMessageDialog(null, "I like your shoes");
+				}
+			else if (randomNumber == 3) {
+				JOptionPane.showMessageDialog(null, "You are amazing");
+				}
+			else if (randomNumber == 4) {
+				JOptionPane.showMessageDialog(null, "You are pretty");
+				}
+			else if (randomNumber == 5) {
+				JOptionPane.showMessageDialog(null, "You are very smart");
+				}
+			else {
+				JOptionPane.showMessageDialog(null, "Your are a fast runner");
+			}
+		}
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
